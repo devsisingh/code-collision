fdb5e28cac8af0b9a4ac6c1dafbd9f8da7d9d4ba
Ayush <ayushagarwal.dev@gmail.com>
2024-08-26 18:16:27 +0530

    add changes

--- src/app/api/idea/[id]/route.ts
+++ src/app/api/idea/[id]/route.ts
@@ -1,36 +1,47 @@
 import { NextRequest, NextResponse } from 'next/server';
 import client from '@/db';
-import {z} from 'zod';
+import { z } from 'zod';
 import { verifyJWT } from '@/app/api/backendUtils';
 
 const updateIdeaSchema = z.object({
-  title:z.string(),
-  description:z.string(),
-})
+  title: z.string(),
+  description: z.string(),
+});
 
-export async function GET(request:NextRequest){
-  const searchParams = request.nextUrl.pathname.split('/');
-  const ideaId = searchParams[searchParams.length-1];
-  const idea =  await client.idea.findUnique({where:{id:Number(ideaId)}});
-  return NextResponse.json({idea });
+export async function GET(request: NextRequest) {
+  try {
+    const ideaId = request.nextUrl.pathname.split('/').pop();
+    const idea = await client.idea.findUnique({
+      where: { id: ideaId },
+    });
+    return NextResponse.json({ idea });
+  } catch (err) {
+    return NextResponse.json({ msg: 'Something went wrong!' }, { status: 500 });
+  }
 }
 
 // Update an Idea
-export async function PATCH(request:NextRequest){
-  const searchParams = request.nextUrl.pathname.split('/');
-  const ideaId = searchParams[searchParams.length-1];
+export async function PATCH(request: NextRequest) {
+  const ideaId = request.nextUrl.pathname.split('/').pop();
   const body = await request.json();
-  const parsedBody = updateIdeaSchema.safeParse(body)
+  const parsedBody = updateIdeaSchema.safeParse(body);
   try {
-    verifyJWT()
-  }catch (err){
-    return NextResponse.json({message:'Not Authorised'}, {status:403})
+    verifyJWT();
+  } catch (err) {
+    return NextResponse.json({ message: 'Not Authorised' }, { status: 403 });
   }
 
   if (!parsedBody.success) {
-    return NextResponse.json({msg: 'Invalid Inputs'}, {status: 411})
+    return NextResponse.json({ msg: 'Invalid Inputs' }, { status: 411 });
   }
-  const {title, description} =  parsedBody.data;
-  const idea =  await client.idea.update({where:{id:Number(ideaId)}, data:{title, description}});
-  return NextResponse.json({idea });
+  const { title, description } = parsedBody.data;
+  try {
+    const idea = await client.idea.update({
+      where: { id: ideaId },
+      data: { title, description },
+    });
+    return NextResponse.json({ idea });
+  } catch (err) {
+    return NextResponse.json({ msg: 'Something went wrong!' }, { status: 500 });
+  }
 }
--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -4,18 +4,25 @@ import { verifyJWT } from '@/app/api/backendUtils';
 
 // Get User by ID
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.pathname.split('/');
-
+  const userId = request.nextUrl.pathname.split('/').pop();
   try {
-    verifyJWT()
-  }catch (err){
-    return NextResponse.json({message:'Not Authorised'}, {status:403})
+    verifyJWT();
+  } catch (err) {
+    return NextResponse.json({ message: 'Not Authorised' }, { status: 403 });
   }
-  const userId = searchParams[searchParams.length-1];
-  const user = await client.user.findUnique({where:{id:Number(userId)}, include:{ideas:true}})
-  if(!user){
-    return NextResponse.json({message:"No User found"},{status:404})
+  try {
+    const user = await client.user.findUnique({
+      where: { id: userId },
+      include: { ideas: true },
+    });
+    if (!user) {
+      return NextResponse.json({ message: 'No User found' }, { status: 404 });
+    }
+    return NextResponse.json({ user });
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'Something went wrong!' },
+      { status: 500 }
+    );
   }
-  return NextResponse.json({user });
 }
-
