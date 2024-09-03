7fff3924bdf17e75d6579bb6c814ffa0756ee631
Devsi <grace.emily876@gmail.com>
2024-09-03 19:22:53 +0530

    cards ui changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -103,117 +103,47 @@ const Dashboard = () => {
           </div>
 
           <div
-            className="my-10"
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: '1rem',
-              maxWidth: '100%',
-            }}
+            className="my-10 grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-4"
           >
             {filteredIdeas?.map((idea) => (
               <Link
                 key={idea.id}
                 href={`/ideas/${idea.id}`}
-                className="relative cursor-pointer rounded-lg"
+                className="relative cursor-pointer rounded-lg p-6 border-white/[0.2] border rounded-xl"
                 style={{
                   background:
                     'radial-gradient(circle at top, #539b82, transparent)',
                   transition: 'background 0.5s ease-out',
-                  width: 'calc(50% - 1rem)', // Adjust width to ensure wrapping
-                  boxSizing: 'border-box',
                 }}
                 onMouseMove={handleMouseMove}
                 onMouseLeave={handleMouseLeave}
               >
-                {/*TODO: Replace this 3d card with Normal card or something from UI Library*/}
-                {/*TODO: Show Proper Category Names, make mapping for it*/}
-                <CardContainer className="inter-var">
-                  <CardBody className="relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border-white/[0.2] min-w-[480px] h-auto rounded-xl p-6 border">
-                    <div className={'flex justify-between'}>
-                      <CardItem
-                        translateZ="50"
-                        className="text-xl font-bold text-neutral-600 text-white capitalize"
-                      >
-                        {idea.title}
-                      </CardItem>
-                      <CardItem className="text-xl font-medium border border-white px-3 py-1 bg-gray-700 rounded-full flex justify-center items-center text-white capitalize">
-                        {idea.vote_count}
-                      </CardItem>
-                    </div>
-                    <CardItem
-                      as="p"
-                      translateZ="60"
-                      className="text-sm max-w-sm mt-2 text-neutral-300"
-                    >
-                      {idea.problem_solved.substring(0, 200)}...
-                    </CardItem>
-                    {/* <CardItem translateZ="100" className="w-full mt-4">
-          <img
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem> */}
-                    <div className="flex justify-between items-center mt-10">
-                      <CardItem
-                        translateZ={20}
-                        as={Link}
-                        href="https://twitter.com/mannupaaji"
-                        target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal text-white"
-                      >
-                        Category
-                      </CardItem>
-                      <CardItem
-                        translateZ={20}
-                        as="button"
-                        className="px-4 py-2 rounded-xl bg-white text-black text-xs font-bold"
-                      >
-                        {idea.category}
-                      </CardItem>
-                    </div>
-                  </CardBody>
-                </CardContainer>
-
-                {/* <div className="text-white text-lg font-semibold mb-4">
+                
+                <div className="text-white text-xl font-semibold mb-4 capitalize">
                   {idea.title}
                 </div>
 
+                <div className="text-gray-300 text-sm my-6">
+                  <span className="font-bold"></span>{idea.problem_solved.substring(0, 250)}...
+                </div>
+
                 <div className='flex justify-between text-white items-center'>
-                  <div style={{ fontSize: '12px' }} className="w-1/3">{idea.userId}</div>
-                  <div className="px-2 py-1 rounded -mt-2 w-1/3 text-center" style={{ fontSize: '12px' }}>
+                  <div className="flex gap-4">
+                    <div style={{ fontSize: '15px', color: '#FFCAD4', marginTop:'3px'}}>{idea?.vote_count} ❤️</div>
+                  <div className="uppercase px-4 py-1.5 rounded-lg text-center" style={{ fontSize: '12px', backgroundColor: '#22577A', color: '#5DEBD7' }}>
+                    Vote 👍🏽
+                  </div>
+                  </div>
+                  <div className="px-4 py-1.5 rounded-lg text-center bg-white text-black font-bold" style={{ fontSize: '13px' }}>
                     {idea.category}
                   </div>
-                  <div className="uppercase px-2 py-1 rounded -mt-2 text-center" style={{ fontSize: '11px', backgroundColor: '#22577A', color: '#5DEBD7' }}>
-                    Status
-                  </div>
                 </div>
-
-                <div className="text-gray-300 text-sm mt-6">
-                  <span className="font-bold">Problem it solves: </span>{idea.problem_solved.substring(0, 100)}...
-                </div>
-
-                <div className="text-gray-300 text-sm mt-6">
-                <span className="font-bold">Possible solution: </span>{idea.possible_solution.substring(0, 100)}...
-                </div>
-
-                <div className="text-gray-300 text-sm mt-6">
-                <span className="font-bold">Resources: </span>{idea.resources[0].substring(0, 100)}...
-                </div>
-
-                <div className="text-gray-300 text-sm mt-6">
-                <span className="font-bold">Additional: </span>{idea.additional.substring(0, 100)}...
-                </div> */}
+                
               </Link>
             ))}
-            {filteredIdeas.length === 0 && !loading && (
-              // TODO Done: Add No Idea found image or large text
+            {filteredIdeas?.length === 0 && !loading && (
               <div className="flex flex-col justify-center items-center mx-auto">
               <p className='text-white text-3xl text-center'>No Idea Found 😕</p>
-              <img src="https://img.freepik.com/premium-vector/flat-illustration-people-argument_928395-33.jpg" className='w-2/3 mt-10' />
               </div>
             )}
 
