f97eb97522658feff93c0c77bf67cc8ffbcb5434
Ayush <ayushagarwal.dev@gmail.com>
2024-09-05 18:24:48 +0530

    add ts fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,25 +13,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-        <Head>
-          <title>ShareTos</title>
-          <meta
-            name="description"
-            content="ShareTos is an innovative Web3 platform designed to enable users to share, `vote`, and collaborate on groundbreaking ideas in a decentralized environment. 
+    <html lang="en">
+      <Head>
+        <title>ShareTos</title>
+        <meta
+          name="description"
+          content="ShareTos is an innovative Web3 platform designed to enable users to share, `vote`, and collaborate on groundbreaking ideas in a decentralized environment. 
 Our goal is to create a space where creativity thrives through `real-time communication`, transparent development, and `community-driven` collaboration."
-          />
-        </Head>
-        <body
-          className={`bg-gradient bg-cover bg-no-repeat w-full min-h-screen flex justify-between h-screen flex-col items-center ${inter.className}`}
-        >
-          <div className='w-full'>
+        />
+      </Head>
+      <body
+        className={`bg-gradient bg-cover bg-no-repeat w-full min-h-screen flex justify-between h-screen flex-col items-center ${inter.className}`}
+      >
+        <div className="w-full">
           <Navbar />
           <BackgroundBeamsWithCollision>
+            {/*@ts-ignore*/}
             {children}
           </BackgroundBeamsWithCollision>
-            </div>
-        </body>
-      </html>
+        </div>
+      </body>
+    </html>
   );
 }
